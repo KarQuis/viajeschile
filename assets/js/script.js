eac410a6a6494369fa918546506135a76d279e01
Karla Quispe <karla.a_21@hotmail.com>
2022-01-08 13:50:49 -0300

    navbar fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,16 @@ $(function () {
         interval: 3000
     })
 
+$(document).ready(function(){
+    var $navegador = $('#nav');
+    var previousScroll = 0;
+    $(window).scroll(function(event){
+       var scroll = $(this).scrollTop();
+       if (scroll > previousScroll && scroll > 200){
+           $navegador.addClass('bgcolor');
+       } else {
+           $cabecera.changeClass('bgcolor');
+       }
+       previousScroll = scroll;    });
+ 
+  });
